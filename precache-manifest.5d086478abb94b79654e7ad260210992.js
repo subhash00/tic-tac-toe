d64fd55e70b6e2173d7a85dd776128adb9ab36e0
subhash00 <subhahpathak1991@gmail.com>
2020-09-11 20:38:54 +0530

    Updates

---
+++ precache-manifest.5d086478abb94b79654e7ad260210992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7421184278f2b2ee8b0527172cf90d1f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1ac0b5ae16e83ee2699f",
+    "url": "/tic-tac-toe/static/css/main.cee17af4.chunk.css"
+  },
+  {
+    "revision": "a4f901174144a3fe903b",
+    "url": "/tic-tac-toe/static/js/2.11c41c3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.11c41c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac0b5ae16e83ee2699f",
+    "url": "/tic-tac-toe/static/js/main.44ff8573.chunk.js"
+  },
+  {
+    "revision": "672a2364fa6a80ac384e",
+    "url": "/tic-tac-toe/static/js/runtime-main.2910fab3.js"
+  }
+]);
